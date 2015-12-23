7821935340148acdee7cbb082be61ba4ca00249c
A. A. M. Sharif <A. A. M. Sharif>
2015-12-23 11:22:34 +0600

    Changed filenames

--- java/com/upcomingdevelopers/shopmanagement/InventoryEntryActivity.java
+++ java/com/upcomingdevelopers/shopmanagement/InventoryEntryActivity.java
@@ -1,20 +1,16 @@
 package com.upcomingdevelopers.shopmanagement;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class InventoryEntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.inventory_entry);
     }
 
     @Override
